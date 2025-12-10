b89bbd4326b3ebe2e0a5e16f9e01cc23221e2063
varshitha pothamsetti <23p31a1222@acet.ac.in>
2025-12-10 11:30:25 +0530

    second commit

--- Backend_Development/Routers/studentRouters.js
+++ Backend_Development/Routers/studentRouters.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {getStudents, addStudents} from "../contrillers/studentControllers.js";
+import {getStudents, addStudents} from "../controllers/studentControllers.js";
 const router = express.Router();
 router.get('/get-students', getStudents);
 router.post('/add-students', addStudents);
